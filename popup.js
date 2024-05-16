aec7ef7bc2d6bec6b59cd24dccf0fd20280a0fe1
devsheva <sheshi.mateo01@gmail.com>
2024-05-16 20:45:27 +0200

    chore: add btn listener to load and run script

--- popup.js
+++ popup.js
@@ -1,1 +1,24 @@
 console.log('popup.js loaded')
+
+const loadAndRunScript = (script) => {
+  return (tab) => {
+    console.log('loadAndRunScript.callback', { script, tab })
+  }
+}
+
+const addBtnListener = (id, script) => {
+  document.getElementById(id).addEventListener('click', () => {
+    chrome.tabs.query(
+      {
+        active: true,
+        lastFocusedWindow: true,
+      },
+      ([tab]) => loadAndRunScript(script)(tab)
+    )
+  })
+}
+const run = () => {
+  addBtnListener('add-checkbox', 'add-checkbox.js')
+}
+
+run()
