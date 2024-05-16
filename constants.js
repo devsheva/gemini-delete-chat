52eb0d35920c23e281eca2d84e32e451703d2f58
devsheva <sheshi.mateo01@gmail.com>
2024-05-16 20:55:06 +0200

    chore: load and run listener script

---
+++ constants.js
@@ -0,0 +1,7 @@
+console.log('constants.js loaded')
+const SELECTORS = {
+  conversations: '.conversations-container',
+}
+
+window.scriptsPath = '/scripts'
+window.Selectors = SELECTORS
--- popup.js
+++ popup.js
@@ -1,11 +1,33 @@
 console.log('popup.js loaded')
 
+/**
+ * Load constants than run listener script
+ * @param {string} script
+ * @returns
+ */
 const loadAndRunScript = (script) => {
   return (tab) => {
     console.log('loadAndRunScript.callback', { script, tab })
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tab.id },
+        files: ['constants.js'],
+      },
+      () => {
+        chrome.scripting.executeScript({
+          target: { tabId: tab.id },
+          files: [scriptsPath, script].join('/').split(),
+        })
+      }
+    )
   }
 }
 
+/**
+ *
+ * @param {string} id
+ * @param {string} script
+ */
 const addBtnListener = (id, script) => {
   document.getElementById(id).addEventListener('click', () => {
     chrome.tabs.query(
@@ -17,6 +39,7 @@ const addBtnListener = (id, script) => {
     )
   })
 }
+
 const run = () => {
   addBtnListener('add-checkbox', 'add-checkbox.js')
 }
